81c297d41a14a071e9f04a506426edfcf5689bce
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-24 17:03:03 +0600

    nested route problem

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -3,14 +3,18 @@ import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider';
 import useAdmin from '../hooks/useAdmin';
 import Header from '../Pages/Shared/Header/Header';
+import Particle from '../Pages/Shared/Particle/Particle';
 
 
 const DashboardLayout = () => {
     const { user } = useContext(AuthContext);
-    const [isAdmin] = useAdmin(user?.email)
+    // const [isAdmin] = useAdmin(user?.email)
     return (
         <div>
-            <Header></Header>
+            <div>
+                <Header></Header>
+            </div>
+
             <div className="drawer drawer-mobile">
                 <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
@@ -19,14 +23,17 @@ const DashboardLayout = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 text-base-content">
-                        <li><Link to="/dashboard">My Appointments</Link></li>
-                        {
+                        <li><Link to="/dashboard/myOrders">My Orders</Link></li>
+                        {/* {
                             isAdmin && <>
                                 <li><Link to="/dashboard/allusers">All users</Link></li>
                                 <li><Link to="/dashboard/adddoctor">Add A Doctor</Link></li>
                                 <li><Link to="/dashboard/managedoctors">Manage Doctors</Link></li>
                             </>
-                        }
+                        } */}
+                        <li><Link to="/dashboard/addProducts">Add Products</Link></li>
+                        <li><Link to="/dashboard/myProduct">My Product</Link></li>
+                        <li><Link to="/dashboard/allSeller">All Seller</Link></li>
 
                     </ul>
 
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../../Layout/DashboardLayout";
 import Main from "../../Layout/Main";
+import MyOrders from "../../Pages/Dashboard/MyOrders/MyOrders";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/Signup/SignUp";
@@ -34,30 +35,13 @@ const router = createBrowserRouter([
         path: '/dashboard',
         // errorElement: <DisplayError></DisplayError>,
         element: <PrivateRoute> <DashboardLayout></DashboardLayout> </PrivateRoute>,
-        // children: [
-        //     {
-        //         path: '/dashboard',
-        //         element: <MyAppointment></MyAppointment>
-        //     },
-        //     // {
-        //     //     path: '/dashboard/allusers',
-        //     //     element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
-        //     // },
-        //     // {
-        //     //     path: '/dashboard/adddoctor',
-        //     //     element: <AdminRoute><AddDoctor></AddDoctor></AdminRoute>
-        //     // },
-        //     // {
-        //     //     path: '/dashboard/managedoctors',
-        //     //     element: <AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
-        //     // },
-        //     // {
-        //     //     path: '/dashboard/payment/:id',
-        //     //     element: <AdminRoute> <Payment></Payment> </AdminRoute>,
-        //     //     loader: ({ params }) => fetch(`http://localhost:8000/bookings/${params.id}`)
+        children: [
+            {
+                path: '/dashboard/myOrders',
+                element: <MyOrders></MyOrders>
+            },
 
-        //     // },
-        // ]
+        ]
     }
 ])
 
